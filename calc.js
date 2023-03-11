efce21b9c29b31e6993b443fbbebe0801c72b206
kallan-randall <kallan92@gmail.com>
2023-03-11 13:43:35 -0700

    All implimentations working

--- calc.js
+++ calc.js
@@ -14,6 +14,29 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+	console.log('mathSymbol' , mathSymbol)
+	console.log('num1' , num1)
+	console.log('num2' , num2)
+
+	if (mathSymbol === "+"){
+		console.log(num1 + num2)
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2)
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2)
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2)
+	}
+
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1))
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
